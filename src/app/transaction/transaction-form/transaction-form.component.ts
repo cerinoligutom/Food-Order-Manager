3f64e2f5407e8cbbe966d6214285c2221d56ce94
Grphx <grphxprod@gmail.com>
2018-03-26 14:06:05 +0800

    -updated transaction form, now supports image as icon

--- src/app/transaction/transaction-form/transaction-form.component.ts
+++ src/app/transaction/transaction-form/transaction-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { OptionItem } from '../../shared/custom-input/custom-input.component';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Vendor } from '../../core/services/models';
+import { VendorService } from '../../core/services';
 
 @Component({
   selector: 'app-transaction-form',
@@ -14,24 +16,24 @@ export class TransactionFormComponent implements OnInit {
 
   form: any;
 
-  foods: OptionItem[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
+  vendors: OptionItem[];
+
 
   constructor(
+    private vendorService: VendorService,
     public dialogRef: MatDialogRef<TransactionFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
   ngOnInit() {
+    this.vendorService.getVendors().subscribe(vendor => {
+      this.vendors = vendor.map(x => <OptionItem>{
+        value: x.id.toString(),
+        viewValue: x.name,
+        image: x.image
+      });
+    });
   }
-
-  // onSubmit(form) {
-  //   // let foods = data.map(x=> <OptionItem>{value: x.id, viewValue: x.name});
-  //   console.log(form);
-  // }
 
   onNoClick(): void {
     this.dialogRef.close();
